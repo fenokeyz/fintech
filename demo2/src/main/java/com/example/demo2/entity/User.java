876fe448346538b21ef2da40145784389d9697fc
Navaneeth Suresh <navaneethsuresh21@gmail.com>
2025-01-31 12:15:07 +0530

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo2/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.demo2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int userId;
+	private String name;
+	private String emailId;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+    
+    
+    
+
+}
